f5968567f83bf59bba71ed0fbcba0a3fb5ccf1af
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2021-03-22 13:43:02 -0400

    Added necessary files for the example project

---
+++ ChillAI/Example Project/Scripts/NetworkData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NetworkData
+{
+    public List<List<float[]>> weights;
+    public List<float[]> biases;
+
+    public NetworkData(List<List<float[]>> weights, List<float[]> biases)
+    {
+        this.weights = weights;
+        this.biases = biases;
+    }
+}
